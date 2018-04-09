17cadfec95b00630c97cd21ec4ffbbf2b89ea516
Difreg <lucmerk@web.de>
2018-04-09 10:47:22 +0200

    First Queries

--- src/database/db_1.java
+++ src/database/db_1.java
@@ -6,16 +6,27 @@ import java.sql.*;
 
 public class db_1 {
 
-	public static void main(String [] args) {
-		connect();
+	public static void main(String [] args) throws SQLException {
+
+		ResultSet rückgabe = connect();
+		while(rückgabe.next()) {
+			System.out.println(rückgabe.getString(1) + " | " + rückgabe.getString(2));
+		}
+		rückgabe.close();
 	}
 	
-	public static void connect() {
+	public static ResultSet connect() {
 		Connection con = null;
 		try {
 			Class.forName("com.mysql.jdbc.Driver").newInstance();
 			con = DriverManager.getConnection("jdbc:mysql://lm-bln-flatchat1.clciw2tvdadg.eu-central-1.rds.amazonaws.com?" +"user=root"+"&password=flatchat123");
 			System.out.println(con.isClosed());
+
+
+
+			Statement testStmt = con.createStatement();
+			testStmt.executeUpdate("Insert Into lm_bln_flatchat1.users Values (2, 'Duschek', 'Felicia')");
+			return testStmt.executeQuery("Select * From lm_bln_flatchat1.users;" );
 		}
 		catch(SQLException ex) {
 			System.out.println("SQLException: " + ex.getMessage());
@@ -30,7 +41,8 @@ public class db_1 {
 		} catch (IllegalAccessException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} 
+		}
+		return null;
 	}
 	
 }
